b5e29ed99a10da194b608f6a9d0d4e1f931f0302
Constantine Evans <const@costinet.org>
2021-06-14 03:10:29 +0100

    read qsconnection import

--- src/qslib/__init__.py
+++ src/qslib/__init__.py
@@ -1,10 +1,7 @@
 import sys
+from .qsconnection_async import QSConnectionAsync
 
-if sys.version_info[:2] >= (3, 8):
-    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
-    from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
-else:
-    from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
+from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
 
 try:
     # Change here if project is renamed and does not equal the package name
@@ -14,3 +11,5 @@ except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
 finally:
     del version, PackageNotFoundError
+
+
