928b22a69b4badc69fda82e985a3291982720a8e
Constantine Evans <const@costi.eu>
2021-12-03 04:09:07 +0000

    improve fakeserver tests

--- tests/test_fakeserver.py
+++ tests/test_fakeserver.py
@@ -6,31 +6,96 @@ import re
 import logging
 
 
-async def _fakeserver_runner(sr: asyncio.StreamReader, sw: asyncio.StreamWriter):
-    sw.write(
-        b"READy -session=12345 -product=QuantStudio3_5 -version=1.3.0 -build=001 -capabilities=Index\n"
-    )
-    await sw.drain()
+def crcb(crlist):
+    async def _fakeserver_runner(sr: asyncio.StreamReader, sw: asyncio.StreamWriter):
+        acc = "Guest"
 
-    while not sr.at_eof():
-        line = await sr.readline()
-        print(line)
-        if x := re.match(rb"(\d+) RUNTitle?", line):
-            sw.write(b"OK " + x.group(1) + b" aoeu\n")
-        elif x := re.match(rb"(\d+) ACC", line):
-            sw.write(b"OK " + x.group(1) + b"\n")
-        elif x := re.match(rb"(\d+) QUIT", line):
-            sw.write(b"OK " + x.group(1) + b"\n")
-            await sw.drain()
-            sw.close()
-            return
+        sw.write(
+            b"READy -session=12345 -product=QuantStudio3_5 -version=1.3.0 -build=001 -capabilities=Index\n"
+        )
         await sw.drain()
+
+        while not sr.at_eof():
+            line = await sr.readline()
+            print(line)
+            if x := re.match(rb"(\d+) ACC (\w+)", line, re.IGNORECASE):
+                sw.write(b"OK " + x.group(1) + b"\n")
+                acc = x.group(2).decode()
+                continue
+
+            if x := re.match(rb"(\d+) ACC?", line, re.IGNORECASE):
+                sw.write(
+                    b"OK "
+                    + x.group(1)
+                    + f" -stealth=False -exclusive=False {acc}".encode()
+                    + b"\n"
+                )
+                continue
+
+            if x := re.match(rb"(\d+) QUIT", line, re.IGNORECASE):
+                sw.write(b"OK " + x.group(1) + b"\n")
+                await sw.drain()
+                sw.close()
+                return
+
+            for com, resp in crlist.items():
+                if x := re.match(rb"(\d+) " + re.escape(com.encode()), line):
+                    sw.write(b"OK " + x.group(1) + b" " + resp.encode() + b"\n")
+                    await sw.drain()
+                    break
+
+    return _fakeserver_runner
+
+
+@pytest.mark.asyncio
+async def test_connection():
+    srv = await asyncio.start_server(crcb({}), "localhost", 53533)
+
+    async with srv:
+        m = Machine("localhost", port=53533)
+
+        assert m.connected is False
+
+        m.connect()
+
+        assert m.connected is True
+
+        m.disconnect()
+
+        assert m.connected is False
+
+        m = Machine("localhost", port=53533, connect_now=True)
+
+        assert m.connected is True
+
+
+@pytest.mark.asyncio
+async def test_acc_level_set():
+    srv = await asyncio.start_server(crcb({}), "localhost", 53533)
+
+    async with srv:
+        m = Machine("localhost", port=53533)
+
+        with m:
+            with m.at_access("Observer"):
+                assert m.get_access_level() == ("Observer", False, False)
+
+            m.set_access_level("Observer")
 
 
 @pytest.mark.asyncio
 async def test_runtitle():
-    srv = await asyncio.start_server(_fakeserver_runner, "localhost", 53533)
+    srv = await asyncio.start_server(crcb({"RUNTitle?": "aoeu"}), "localhost", 53533)
 
     async with srv:
         with Machine("localhost", port=53533) as m:
             assert m.current_run_name == "aoeu"
+
+
+@pytest.mark.asyncio
+async def test_runtitle_not_running():
+    srv = await asyncio.start_server(crcb({"RUNTitle?": "-"}), "localhost", 53533)
+
+    async with srv:
+        with Machine("localhost", port=53533) as m:
+            assert m.current_run_name == None
