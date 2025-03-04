99309994dacdd56513dc3bb3e41e75fd79760706
Constantine Evans <constantine@costi.net>
2025-03-03 21:53:14 -0500

    convert to rust/python hybrid

---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+pub mod python;
+use pyo3::prelude::*;
+
+#[pymodule]
+mod qslib_rs {
+    #[pymodule_export]
+    use crate::python::PyQSConnection;
+
+    #[pymodule_export]
+    use crate::python::PyMessageResponse;
+
+    #[pymodule_export]
+    use crate::python::PyLogReceiver;
+
+    #[pymodule_export]
+    use qslib::parser::OkResponse;
+
+    #[pymodule_export]
+    use crate::python::UnexpectedMessageResponse;
+
+    #[pymodule_export]
+    use crate::python::DisconnectedBeforeResponse;
+
+    #[pymodule_export]
+    use crate::python::CommandError;
+
+    #[pymodule_export]
+    use crate::python::QslibException;
+
+    #[pymodule_export]
+    use crate::python::CommandResponseError;
+
+    
+    
+}
