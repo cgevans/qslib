2411549d8c5322966b613f153704ae2754f252a5
Constantine Evans <constantine@costi.net>
2025-11-18 06:05:05 +0000

    more tests

---
+++ tests/test_plate_setup.py
@@ -0,0 +1,16 @@
+import pytest   
+
+from qslib import PlateSetup
+
+def test_plate_setup_equality():
+    ps = PlateSetup({
+        "s1": ["A1"],
+        "s2": ["A2", "B4"],
+    })
+
+    ps2 = PlateSetup({
+        "s1": ["A1"],
+        "s2": ["A2", "B4"],
+    })
+    assert ps == ps2
+    
